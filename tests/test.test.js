1e9b1abf0f0dc30c3f17eb47a8579171b189bf2b
Eitan <eg12092004@gmail.com>
2025-06-18 18:46:54 +0300

    started electron

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,10 +1,22 @@
-import { test, expect } from '@playwright/test';
+const { test, expect } = require('@playwright/test');
+const { emulateToggleDeviceToolbar } = require('../utils/setupTabletCDP');
 
+let browser, context, page;
+let client;
 
-test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-});
+test.describe('Tablet Toggle Toolbar Mode', () => {
+    test.beforeEach(async () => {
+        ({ browser, context, page } = await emulateToggleDeviceToolbar());
+        client = await context.newCDPSession(page);
 
-test('main test', async ( {page} ) => {
-    await page.waitForTimeout(5000);
+        await page.goto('/');
+    });
+
+    test('Basic tablet behavior', async () => {
+        await page.waitForTimeout(15000); // For manual interaction check
+    });
+
+    test.afterEach(async () => {
+        await browser?.close();
+    });
 });
